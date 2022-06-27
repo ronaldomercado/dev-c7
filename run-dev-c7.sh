9e52d6f8dfef796128a2d77bd78716553675bb1a
Giles Knap <giles.knap@diamond.ac.uk>
2022-06-27 07:46:11 +0000

    improved launcher script

--- run-dev-c7.sh
+++ run-dev-c7.sh
@@ -2,42 +2,64 @@
 
 # A script for launching 'RHEL 7 in a Box' on a DLS workstation
 
-while getopts "hs:" arg; do
-  case $arg in
-    s)
-      hostname=$OPTARG
-      ;;
-    *)
-      echo "usage:
-
-      run-dev.sh [options]
-
-      Launches a developer container that simulates a DLS RHEL7 workstation.
-
-      options:
-        -h:  show this help       
-        -s:  set a hostname for your container (dev-c7)
-      "
-      exit 0
-      ;;
-  esac
-done
-
 # NOTE that changes to this file should also be propgated to .devcontainer.json
 
-image=ghcr.io/dls-controls/dev-c7:latest
+image=ghcr.io/dls-controls/dev-c7
+version=latest
+hostname=
+changed=
+
+while getopts "hs:i:v:" arg; do
+    case $arg in
+    s)
+        hostname=$OPTARG
+        changed=true
+        ;;
+    i)
+        image=$OPTARG
+        changed=true
+        ;;
+    v)
+        version=$OPTARG
+        changed=true
+        ;;
+    *)
+        echo "usage:
+
+        run-dev.sh [options]
+
+        Launches a developer container that simulates a DLS RHEL7 workstation.
+
+        options:
+            -h:          show this help       
+            -i  image   ("${image}")
+            -v:  version ("${version}")
+            -s:  set a hostname for your container (dev-c7)
+        "
+        exit 0
+        ;;
+    esac
+done
+
+
+if [[ -e /etc/centos-release ]] ; then
+    echo "ERROR: already in the a devcontainer"
+    exit 1
+fi
 
 environ="-e DISPLAY -e HOME"
-volumes="-v /dls_sw/prod:/dls_sw/prod \
-        -v /dls_sw/work:/dls_sw/work \
-        -v /dls_sw/epics:/dls_sw/epics \
-        -v /dls_sw/targetOS/vxWorks/Tornado-2.2:/dls_sw/targetOS/vxWorks/Tornado-2.2 \
-        -v /dls_sw/apps:/dls_sw/apps \
-        -v /dls_sw/etc:/dls_sw/etc \
-        -v /scratch:/scratch \
-        -v /home:/home \
-        -v /tmp:/tmp \
-        -v /dls/science/users/:/dls/science/users/"
+volumes=" 
+    -v /dls_sw/prod:/dls_sw/prod
+    -v /dls_sw/work:/dls_sw/work
+    -v /dls_sw/epics:/dls_sw/epics
+    -v /dls_sw/targetOS/vxWorks/Tornado-2.2:/dls_sw/targetOS/vxWorks/Tornado-2.2
+    -v /dls_sw/apps:/dls_sw/apps
+    -v /dls_sw/etc:/dls_sw/etc
+    -v /scratch:/scratch
+    -v /home:/home
+    -v /tmp:/tmp
+    -v /dls/science/users/:/dls/science/users/
+"
 
 devices="-v /dev/ttyS0:/dev/ttyS0"
 opts="--net=host --hostname ${hostname:-dev-c7}"
@@ -46,7 +68,7 @@ identity="--security-opt=label=type:container_runtime_t --userns=keep-id \
           --annotation run.oci.keep_original_groups=1"
 
 # this runtime is also required for secondary groups
-if which crun 2> /dev/null ; then 
+if which crun &> /dev/null ; then 
     runtime="--runtime /usr/bin/crun"
     identity="${identity} --storage-opt ignore_chown_errors=true"
 fi
@@ -63,15 +85,17 @@ container_name=dev-c7
 # preserved unless an explict 'podman rm dev-c7' is invoked.
 ################################################################################
 
-if [ "$(podman ps -q -f name=${container_name})" ]; then
-    : # container already running so no prep required
-    if [[ ${hostname} ]] ; then
-        echo "ERROR: cannot change hostname on a running container."
+if [[ -n $(podman ps -q -f name=${container_name}) ]]; then
+    # container already running so no prep required   
+    if [[ -n ${changed} ]] ; then
+        echo "ERROR: cannot change hostname or image on a running container."
         echo "Delete the container with 'podman rm -ft0 dev-c7' and retry."
         exit 1
-    fi
-elif [ "$(podman ps -qa -f name=${container_name})" ]; then
+    fi 
+    echo 'attaching to exisitng dev-c7 container ...'
+elif [[ -n $(podman ps -qa -f name=${container_name}) ]]; then
     # start the stopped container
+    echo 'restarting stopped dev-c7 container ...'
     podman start ${container_name}
 else
     # create a new background container making process 1 be 'sleep'
@@ -80,8 +104,8 @@ else
     # /etc/adduser.conf
     echo 'creating new dev-c7 container ...'
     podman run -d --name ${container_name} ${runtime} ${environ}\
-        ${identity} ${volumes} ${devices} ${opts} ${image} \
-        bash -c "sudo sed -i s#/bin/sh#/bin/bash# /etc/passwd ; sleep 100d"
+        ${identity} ${volumes} ${devices} ${opts} ${image}:${version} \
+        bash -c "sudo sed -i s#/bin/sh#/bin/bash# /etc/passwd ; sleep 1000d"
 fi
 # Execute a shell in the container - this allows multiple shells and avoids 
 # using process 1 so users can exit the shell without killing the container
